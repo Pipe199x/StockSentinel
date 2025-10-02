1a70eb02b8f220f45a353cf5b0e495735bd2bac8
Pipe199x <duqueandres800@gmail.com>
2025-10-02 15:40:05 -0500

    Listo para producción

--- sorata.py
+++
@@ -1,4 +0,0 @@
-# prueba rápida para ver si tu CSV tiene dividendos > 0 por ticker
-import pandas as pd
-df = pd.read_csv("data/AMZN_prices_20251001.csv")  # cambia nombre al último archivo MSFT
-print(df[df["dividends"] > 0][["date","dividends"]].head(10))
